f05da3e54d4578a4559a7bb6b249708e0f8a52a5
Wesley Hott Soares <wesley.hott@gmail.com>
2019-10-25 21:30:08 -0300

    feat: autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'hello world' });
 });
+
+routes.get('/users', UserController.index);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware); // somente as rotas que vem depois usam esse middleware
+routes.put('/users', UserController.update);
 
 export default routes;
